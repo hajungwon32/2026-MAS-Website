8dfd94d74cfe7542f0acfe6a3230a2ff67ec242d
hajungwon32 <netrock49@gmail.com>
2026-02-28 00:21:59 +0900

    [Style]:: MainMembers 및 MainProjects 페이지 반응형 수정

--- src/pages/MainMembers.jsx
+++ src/pages/MainMembers.jsx
@@ -13,10 +13,19 @@ function MainMembers() {
               <p>MAS를 빛내고 지금까지 <br />
                 이끌어 온 부원들입니다</p>
             </div>
-          {/* 이거 누르면 members 페이지로 이동함 */}
+
           <Link to="/members" className='members-btn'>더 보기 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" viewBox="0 0 18 16" fill="none">
           <path d="M1 8H17M17 8L9.8 1M17 8L9.8 15" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           </svg></Link>
+
+          {/* 모바일 전용 더 보기 */}
+          <div className='members-btn-M'>
+            <Link to="/members">더보기
+            <svg xmlns="http://www.w3.org/2000/svg" width="8" height="16" viewBox="0 0 8 16" fill="none">
+            <path d="M0.749999 14.75L6.08061 8.53095C6.4658 8.08156 6.4658 7.41844 6.08061 6.96905L0.749999 0.750001" stroke="white" strokeWidth="1.5" strokeLinecap="round"/>
+            </svg></Link>
+          </div>
+
         </div>
         {/* direction 슬라이드 방향
         normal: 오른쪽 -> 왼쪽   reverse: 왼쪽 -> 오른쪽 */}
--- src/pages/MainProjects.jsx
+++ src/pages/MainProjects.jsx
@@ -16,14 +16,26 @@ function MainProjects() {
           <p>서로의 능력을 모아 함께 완성한 <br />
             MAS의 프로젝트들을 만나보세요</p>
         </div>
-        {/* 자세히 보기 누르면 프로젝트 화면으로 이동 */}
+
         <Link to="/projects" className='projects-btn'>자세히 보기 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" viewBox="0 0 18 16" fill="none">
         <path d="M1 8H17M17 8L9.8 1M17 8L9.8 15" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
         </svg></Link>
+
+        {/* 모바일 전용 버튼 */}
+        <div className='projects-btn-M'>
+          <Link to="/projects">더보기
+          <svg xmlns="http://www.w3.org/2000/svg" width="8" height="16" viewBox="0 0 8 16" fill="none">
+          <path d="M0.749999 14.75L6.08061 8.53095C6.4658 8.08156 6.4658 7.41844 6.08061 6.96905L0.749999 0.750001" stroke="white" strokeWidth="1.5" strokeLinecap="round"/>
+          </svg></Link>
+        </div>
+
       </div>
       <div className='sliders'>
         <Slider slideList={slideProjectsUp} direction="reverse" name='projects-slideUp'/>
         <Slider slideList={slideProjectsDown} direction="normal" name='projects-slideDown'/>
+
+        {/* 모바일 슬라이드 */}
+        <Slider slideList={projectsList} direction="reverse" name='projects-mobile'/>
       </div>
     </div>  
   );
